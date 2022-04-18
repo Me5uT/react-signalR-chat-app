3a4e6a1eb6397e2a6c204ef12e2a795499007d98
Me5uT <me5ut@windowslive.com>
2022-04-19 02:23:55 +0300

    Client: Fixed Sending Message Bug

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,22 @@
+import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
 import React from "react";
 import "./App.css";
-import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
 import { Chat } from "./Chat";
 
 const URL = "http://localhost:5000/chat";
 
 export const App = () => {
   const [hubConnection, setHubConnection] = React.useState<HubConnection>();
-  const [text, setText] = React.useState<string>("");
-  const [messageList, setMessageList] = React.useState<string[]>([]);
   const [showChat, setShowChat] = React.useState<boolean>(false);
   const [room, setRoom] = React.useState<string>("");
   const [userName, setUserName] = React.useState<string>("");
-
-  // React.useEffect(() => {
-  //   createHubConnection();
-  // }, []);
-
-  React.useEffect(() => {
-    if (hubConnection) {
-      hubConnection.on("ReceiveMessage", (message) => {
-        setMessageList([...messageList, message]);
-        console.log("message", message);
-      });
-    }
-  }, [hubConnection]);
 
   const createHubConnection = async () => {
     const hubConnection = new HubConnectionBuilder().withUrl(URL).build();
 
     try {
       await hubConnection.start();
-      console.log("Bağlandı.");
+      console.log(userName, " bağlandı.");
     } catch (error) {
       console.log("hata:", error);
     }
@@ -39,20 +24,11 @@ export const App = () => {
     setHubConnection(hubConnection);
   };
 
-  const sendMessage = async () => {
-    if (hubConnection) {
-      await hubConnection.invoke("SendMessage", text);
-    }
-  };
-
   const joinRoom = () => {
     if (userName !== "" && room !== "") {
-      // socket.emit("join_room", room);
       createHubConnection();
       setShowChat(true);
     }
-
-    setMessageList((list) => [...list, text]);
   };
 
   return (
@@ -72,6 +48,9 @@ export const App = () => {
             placeholder="Room ID ..."
             onChange={(event) => {
               setRoom(event.target.value);
+            }}
+            onKeyPress={(event) => {
+              event.key === "Enter" && joinRoom();
             }}
           />
           <button onClick={joinRoom}>Join A Room</button>
